db8fdc7a6627c221ab99e2bd9c15050e1538a91d
Edwin Lin <edwinlin@gmail.com>
2018-11-07 07:58:06 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -12,9 +12,22 @@ const codes = [
 ];
 
 function init() {
-  }
+function init() {
+  let idx = 0
 
-  body.addEventListener('keydown', onKeyDownHandler);
+  document.body.addEventListener("keydown", (e) => {
+    const key = e.key
+
+    idx = (codes[idx] === key) ? ++idx : 0
+
+
+    if (idx === codes.length) {
+      window.alert("Hurray!");
+      idx = 0
+    }
+
+  });
+}
 
   
 }
