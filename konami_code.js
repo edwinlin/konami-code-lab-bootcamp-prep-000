56f0dedd17d03c9a5fbc6cdb4340f6a17e36896e
Edwin Lin <edwinlin@gmail.com>
2018-11-07 07:56:25 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -29,7 +29,7 @@ function init() {
         if(counter === codes.length){
           counter = 0
           alert("Hurray!")
-          // windows.alert("Hurray!")
+          windows.alert("Hurray!")
           console.log("Hurray!")
         }
 
