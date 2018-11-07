52955ec2c0aab79f32fa9b59984ed799b7bfc5e9
Edwin Lin <edwinlin@gmail.com>
2018-11-07 08:06:55 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -15,21 +15,27 @@ function init() {
   // your code here
   var body = document.body
   
-  // let counter = 0
-  let idx = 0
+  let counter = 0
 
   function onKeyDownHandler(e){
+    console.log(e.key)
 
-    const key = e.key
+    const key = e.key;
 
-    idx = (codes[idx] === key) ? ++idx : 0
+      if(key === codes[counter]){
+        counter += 1
+        console.log(counter)
 
+        if(counter === codes.length){
+          counter = 0
+          alert("Hurray!")
+          console.log("Hurray!")
+        }
 
-    if (idx === codes.length) {
-      window.alert("Hurray!");
-      idx = 0
-    }
-
+      }else{
+        counter = 0
+        console.log('back to 0')
+      }
   }
 
   body.addEventListener('keydown', onKeyDownHandler);
